5eccc8b848f629673a8d0d569043f4d708401bb8
Eric 'Zander' Nelson <ci.eric.nelson@gmail.com>
2014-01-13 14:21:14 -0800

    comments and documentation
    added comments to the renamed 'rename.py' file
    added documentation to the readme for 'rename.py'

--- folder-casechange/README.md
+++ folder-casechange/README.md
@@ -1,5 +1,18 @@
 Eric Nelson
 
+rename.py
+===========
+
+This program allows the quick renaming of files and folders to comply with specific naming conventions.  It changes said files and folders to lower case and replaces unecessary characters such as '&' and '_' with dashes (-).  It queries the user to whether to change just folders or to include files and whether it should include subdirectores.
+
+1. Place the python file 'rename.py' in the folder that has the contents that you would wish to change to lower case.
+2. Run the python program.
+3. Answer the prompts.
+4. ???
+5. Profit!
+
+
+
 folder-lowercase
 subfolder-lowercase
 file-lowercase
--- folder-casechange/rename.py
+++ folder-casechange/rename.py
@@ -2,6 +2,7 @@
 import os, re, time
 
 # rename function
+# renames files/folders to lowercase
 def rename(myFile):
 	name = ''
 	count = 0
@@ -22,6 +23,7 @@ def rename(myFile):
 	# returns new name
 	return name
 
+# walk function
 def walk(justFolder, sub):
 	if sub:
 		for path, folders, files in os.walk('.'):
@@ -35,48 +37,42 @@ def walk(justFolder, sub):
 					os.rename(os.path.join(path, aFile), os.path.join(path, name))
 
 	else:
-		# for each folder in current directory
+		# for each file or folder in current directory
 		for folder in os.listdir('.'):
 			if os.path.isdir(folder) or not justFolder:	# if is a folder or checking all files
-				name = ''
-				count = 0
-
-				# regex, converts to lower and removes characters
-				# saves what is kept into an array
-				q = re.split('[-_&$#]', folder.lower())
-
-				# for each part in the array, add to new name
-				for z in q:
-					if z != '':
-						name += z
-						if count < len(q) - 1:
-							name += '-'
-
-					count += 1
-
-				# renames file
+				name = rename(folder)
 				os.rename(folder, name)
 
 
+# interface
+# determines if the user wants to rename files and folders or just folders
+looping = True
+while looping:
+	result = raw_input("Rename only folders? (Yes/No")
+	result = result.lower()
+	if result == 'y' or result == 'yes':
+		foldersOnly = True
+		looping = False
+	elif result == 'n' or result == 'no':
+		foldersOnly = False
+		looping = False
+	else:
+		print "Please enter either yes or no"
 
-result = raw_input("Rename only folders?")
-result = result.lower()
-if result == 'y' or result == 'yes':
-	foldersOnly = True
-elif result == 'n' or result == 'no':
-	foldersOnly = False
-else:
-	print "nope"
-
-result = raw_input("Include Subdirectories?")
-result = result.lower()
-if result == 'y' or result == 'yes':
-	subdirectories = True
-elif result == 'n' or result == 'no':
-	subdirectories = False
-else:
-	print "nope"
+# determines if the user wants to include subdirectories
+looping = True
+while looping:
+	result = raw_input("Include Subdirectories?")
+	result = result.lower()
+	if result == 'y' or result == 'yes':
+		subdirectories = True
+		looping = False
+	elif result == 'n' or result == 'no':
+		subdirectories = False
+		looping = False
+	else:
+		print "Please enter either yes or no"
 
 walk(foldersOnly, subdirectories)
 
-time.sleep(5)
+# time.sleep(5)
