06a47185a1c2da3670db7d3b0e98f87bd9141b8c
Eric 'Zander' Nelson <ci.eric.nelson@gmail.com>
2014-01-15 13:19:42 -0800

    added the removal of spaces

--- folder-casechange/rename.py
+++ folder-casechange/rename.py
@@ -9,7 +9,7 @@ def rename(myFile):
 
 	# regex, converts to lower and removes characters
 	# saves what is kept into an array
-	q = re.split('[-_&$#]', myFile.lower())
+	q = re.split('[-_&$# ]', myFile.lower())
 
 	# for each part in the array, add to new name
 	for z in q:
